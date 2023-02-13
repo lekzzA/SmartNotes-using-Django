c415928c0252b7a5b2c498c3732f37f98a2b67c9
Tanuj Bhatt <bhatttanuj3@gmail.com>
2023-02-13 13:35:39 +0530

    Adds specific URLs for detailed Note View

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Notes
+from django.http import Http404
+
+def listnote(request):
+    all_notes = Notes.objects.all()
+    return render(request, 'notes/note_list.html', {'notes': all_notes})
+
+def detail(request, pk):
+    try:
+        note = Notes.objects.get(pk=pk)
+    except Notes.DoesNotExist:
+        raise Http404("Note doesn't exist")
+    return render(request, 'notes/notes_details.html', {'note': note})
