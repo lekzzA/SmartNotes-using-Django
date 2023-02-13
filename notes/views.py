dd06e000e3815d3704a55db2adb85c65dd6e4200
Tanuj Bhatt <bhatttanuj3@gmail.com>
2023-02-13 18:43:12 +0530

    Introduces Class-based views

--- notes/views.py
+++ notes/views.py
@@ -3,15 +3,26 @@ from django.shortcuts import render
 # Create your views here.
 from .models import Notes
 from django.http import Http404
+from django.views.generic import ListView,DetailView
 
-def listnote(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/note_list.html', {'notes': all_notes})
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = 'notes/note_list.html'
 
-def detail(request, pk):
-    try:
-        note = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        return render(request, 'notes/404_Exception.html', {})
-        # raise Http404("Note doesn't exist")
-    return render(request, 'notes/notes_details.html', {'note': note})
+# def listnote(request):
+#     all_notes = Notes.objects.all()
+#     return render(request, 'notes/note_list.html', {'notes': all_notes})
+
+class DetailedView(DetailView):
+    model = Notes
+    context_object_name = ("note")
+    template_name = "notes/notes_details.html"
+
+# def detail(request, pk):
+#     try:
+#         note = Notes.objects.get(pk=pk)
+#     except Notes.DoesNotExist:
+#         return render(request, 'notes/404_Exception.html', {})
+#         # raise Http404("Note doesn't exist")
+#     return render(request, 'notes/notes_details.html', {'note': note})
