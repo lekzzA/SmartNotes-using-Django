8964783dca4a7b8fee73d63ab303f108be1dbfd1
Tanuj Bhatt <bhatttanuj3@gmail.com>
2023-02-15 14:48:49 +0530

    Creates Front-end of the Application
    Uses block-content and utilizes base.html as the basic html file.
    Use of Static content in separate static folder.
    Including static directory in settings
    Gives names to the URLs.

--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.NotesListView.as_view()),
-    path('notes/<int:pk>', views.DetailedView.as_view()),
+    path('notes', views.NotesListView.as_view(), name="note.list"),
+    path('notes/<int:pk>', views.DetailedView.as_view(), name="note.detail"),
 ]
